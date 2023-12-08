4292cdc98daf2bb96248980c2a760c28e60a6883
Devmatrix <devmatrix73@gmail.com>
2023-12-08 17:51:23 +0100

    Closes #3 - Add user firstname - US#4

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,17 @@
+import { useMockedData } from '../../hooks/useMockedData';
+import { USER_MAIN_DATA } from '../../mocks/userData';
+
 function Profile() {
+  const { data } = useMockedData(18, USER_MAIN_DATA);
+
+  const firstName = data && data.userInfos.firstName;
+
   return (
     <div className="profile">
       <div>
         <h1>
           <span>Bonjour</span>
-          <span>Thomas</span>
+          <span>{firstName}</span>
         </h1>
         <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
         <div className="graphics"></div>

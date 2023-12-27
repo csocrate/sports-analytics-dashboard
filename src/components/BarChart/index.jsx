cda86128d67d9f8bc95418dcc136fbe4741a0b33
Devmatrix <devmatrix73@gmail.com>
2023-12-27 20:11:12 +0100

    Ref. #10 - Update BarChart with new datas, bars and styled axis - #US11

--- src/components/BarChart/index.jsx
+++ src/components/BarChart/index.jsx
@@ -2,18 +2,38 @@ import PropTypes from 'prop-types';
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-const margin = { top: 30, bottom: 50, left: 30, right: 50 };
+const margin = { top: 10, bottom: 20, left: 10, right: 40 };
 
 function BarChart({ datas, width, height }) {
   const svgRef = useRef(null);
 
-  const barChartDatas = d3.map(new Set(datas), (d) => {
+  const datasUpdated = d3.map(new Set(datas), (d) => {
     const day = new Date(d.day);
     return {
       ...d,
       day: day.getDate(),
     };
   });
+  const barChartDatas = d3.reduce(
+    new Set(datasUpdated),
+    (acc, d) => {
+      acc.push({
+        day: d.day,
+        value: d.kilogram,
+        name: 'Poids(kg)',
+        color: '#000',
+      });
+      acc.push({
+        day: d.day,
+        value: d.calories,
+        name: 'Calories brûlées(kCal)',
+        color: '#E60000',
+      });
+      return acc;
+    },
+    [],
+  );
+  // console.log(barChartDatas);
 
   // Declares the chart dimensions and margins.
   const boundWidth = width - margin.right - margin.left;
@@ -23,35 +43,69 @@ function BarChart({ datas, width, height }) {
     const svg = d3.select(svgRef.current);
     svg.selectAll('*').remove();
 
-    // Declares the x (horizontal position) scale.
-    const x = d3.scaleUtc(
-      d3.extent(barChartDatas, (d) => d.day),
-      [margin.left, boundWidth - margin.right],
-    );
-    const xAxis = d3.scaleUtc(
-      d3.extent(barChartDatas, (d) => d.day),
-      [margin.left, boundWidth - margin.left],
-    );
+    // List of names
+    const names = new Set(barChartDatas.map((d) => d.name));
 
-    // Declares the y (vertical position) scale.
-    const y = d3
-      .scaleLinear()
-      .domain(d3.extent(barChartDatas, (d) => d.kilogram))
-      .range([boundHeight, 0]);
-    const yAxis = d3.axisRight(y);
-
-    // Adds the x-axis
+    // Adds X axis
+    const fx = d3
+      .scaleBand()
+      .domain(new Set(barChartDatas.map((d) => d.day)))
+      .rangeRound([margin.left, boundWidth - margin.right])
+      .paddingInner(0.1);
+    const x = d3
+      .scaleBand()
+      .domain(names)
+      .rangeRound([0, fx.bandwidth()])
+      .padding(0.75);
     svg
       .append('g')
-      .attr('transform', `translate(0, ${boundHeight})`)
-      .call(d3.axisBottom(x).scale(xAxis).tickSize(0).tickPadding(20));
+      .attr('transform', `translate(0,${boundHeight - margin.top})`)
+      .call(d3.axisBottom(fx).tickSize(0).tickPadding(25))
+      .call((g) => g.selectAll('.domain').remove())
+      .selectAll('.tick text')
+      .style('font-size', '14px');
 
-    // Adds the y-axis
+    // Adds Y axis
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(barChartDatas, (d) => d.value)])
+      .nice()
+      .rangeRound([boundHeight + margin.bottom, margin.top]);
     svg
       .append('g')
       .attr('transform', `translate(${boundWidth}, 0)`)
-      .call(yAxis.tickSize(0));
-    console.log(barChartDatas);
+      .call(d3.axisRight(y).tickSize(0))
+      .call((g) => g.selectAll('.domain').remove())
+      .call((g) =>
+        g
+          .selectAll('.tick line')
+          .attr(
+            'transform',
+            `translate(-${boundWidth + margin.right + margin.left}, -0)`,
+          )
+          .attr('x2', boundWidth + margin.right + margin.left)
+          .attr('stroke-opacity', 0.5)
+          .attr('stroke-dasharray', '2,2'),
+      )
+      .selectAll('.tick text')
+      .style('font-size', '14px');
+
+    // Appends a group for each day, and a rect for each name
+    svg
+      .append('g')
+      .selectAll()
+      .data(d3.group(barChartDatas, (d) => d.day))
+      .join('g')
+      .attr('transform', ([day]) => `translate(${fx(day)},0)`)
+      .selectAll()
+      .data(([, d]) => d)
+      .join('rect')
+      .attr('x', (d) => x(d.name))
+      .attr('y', (d) => y(d.value))
+      .attr('ry', 5)
+      .attr('width', x.bandwidth())
+      .attr('height', (d) => y(0) - y(d.value))
+      .attr('fill', (d) => d.color);
   }, [barChartDatas, boundHeight, boundWidth]);
 
   return (

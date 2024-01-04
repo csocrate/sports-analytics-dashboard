f0ef7fcb884accc9b4186f0cd4c775cd0db8445f
Devmatrix <devmatrix73@gmail.com>
2024-01-04 16:14:43 +0100

    Ref. #12 - Update RadarChart by adding polygon svg instead of line - #US13

--- src/components/RadarChart/index.jsx
+++ src/components/RadarChart/index.jsx
@@ -5,14 +5,14 @@ import { useEffect, useRef } from 'react';
 function RadarChart({ datas, kinds, width, height }) {
   const svgRef = useRef(null);
 
-  console.log(datas);
-  console.log(kinds);
+  // console.log(datas);
+  // console.log(kinds);
 
   const axisConfig = Object.values(kinds).map(
     (el) => el[0].toUpperCase() + el.slice(1),
   );
   axisConfig.unshift(axisConfig.pop());
-  console.log(axisConfig);
+  // console.log(axisConfig);
 
   // Defines ratio datas
   const maxValue = d3.max(datas, (d) => d.value);
@@ -25,121 +25,106 @@ function RadarChart({ datas, kinds, width, height }) {
     return acc;
   }, []);
   ratioDatas.unshift(ratioDatas.pop());
-  console.log(ratioDatas);
+  // console.log(ratioDatas);
 
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const numPoints = 6;
-  const radius = height / 3;
-  const points = [
-    {
-      x:
-        Math.sin((0 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[0] +
-        centerX,
-      y:
-        Math.cos((0 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[0] +
-        centerY,
-    },
-    {
-      x:
-        Math.sin((1 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[1] +
-        centerX,
-      y:
-        Math.cos((1 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[1] +
-        centerY,
-    },
-    {
-      x:
-        Math.sin((2 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[2] +
-        centerX,
-      y:
-        Math.cos((2 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[2] +
-        centerY,
-    },
-    {
-      x:
-        Math.sin((3 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[3] +
-        centerX,
-      y:
-        Math.cos((3 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[3] +
-        centerY,
-    },
-    {
-      x:
-        Math.sin((4 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[4] +
-        centerX,
-      y:
-        Math.cos((4 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[4] +
-        centerY,
-    },
-    {
-      x:
-        Math.sin((5 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[5] +
-        centerX,
-      y:
-        Math.cos((5 / numPoints) * Math.PI * 2 + Math.PI) *
-          radius *
-          ratioDatas[5] +
-        centerY,
-    },
-  ];
-  const lines = d3.range(numPoints).map((i) => ({
-    x1: points[i].x,
-    y1: points[i].y,
-    x2: points[(i + 1) % numPoints].x,
-    y2: points[(i + 1) % numPoints].y,
-  }));
+  const centerX = width / 2,
+    centerY = height / 2,
+    numPoints = 6,
+    radius = height / 3,
+    points = [
+      {
+        x:
+          Math.sin((0 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[0] +
+          centerX,
+        y:
+          Math.cos((0 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[0] +
+          centerY,
+      },
+      {
+        x:
+          Math.sin((1 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[1] +
+          centerX,
+        y:
+          Math.cos((1 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[1] +
+          centerY,
+      },
+      {
+        x:
+          Math.sin((2 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[2] +
+          centerX,
+        y:
+          Math.cos((2 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[2] +
+          centerY,
+      },
+      {
+        x:
+          Math.sin((3 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[3] +
+          centerX,
+        y:
+          Math.cos((3 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[3] +
+          centerY,
+      },
+      {
+        x:
+          Math.sin((4 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[4] +
+          centerX,
+        y:
+          Math.cos((4 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[4] +
+          centerY,
+      },
+      {
+        x:
+          Math.sin((5 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[5] +
+          centerX,
+        y:
+          Math.cos((5 / numPoints) * Math.PI * 2 + Math.PI) *
+            radius *
+            ratioDatas[5] +
+          centerY,
+      },
+    ],
+    polygonPoints = points.map((point) => `${point.x},${point.y}`);
+  // console.log(polygonPoints);
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
     svg.selectAll('*').remove();
 
-    svg.attr('fill', '#282D30');
-
-    //Draw the Circle
+    //Draws the Circle
     svg
       .append('circle')
       .attr('cx', centerX)
       .attr('cy', centerY)
       .attr('r', radius)
-      .style('stroke', 'black')
-      .style('fill', 'none')
-      .attr('stroke-width', 2);
-    //Add polygon
-    svg
-      .selectAll('line')
-      .data(lines)
-      .enter()
-      .append('line')
-      .attr('x1', (d) => d.x1)
-      .attr('y1', (d) => d.y1)
-      .attr('x2', (d) => d.x2)
-      .attr('y2', (d) => d.y2)
-      .attr('stroke', 'blue')
-      .attr('stroke-width', 2);
-    //Add labels
+      .style('stroke', 'white')
+      .style('fill', 'none');
+
+    //Adds polygon
+    svg.append('polygon').attr('points', polygonPoints);
+
+    //Adds labels
     svg
       .append('text')
       .attr('text-anchor', 'middle')
@@ -161,11 +146,13 @@ function RadarChart({ datas, kinds, width, height }) {
         'x',
         Math.sin((1 / numPoints) * Math.PI * 2 + Math.PI) * radius +
           centerX -
-          35,
+          30,
       )
       .attr(
         'y',
-        Math.cos((1 / numPoints) * Math.PI * 2 + Math.PI) * radius + centerX,
+        Math.cos((1 / numPoints) * Math.PI * 2 + Math.PI) * radius +
+          centerX +
+          10,
       )
       .text(axisConfig[1]);
     svg
@@ -175,13 +162,11 @@ function RadarChart({ datas, kinds, width, height }) {
         'x',
         Math.sin((2 / numPoints) * Math.PI * 2 + Math.PI) * radius +
           centerX -
-          25,
+          30,
       )
       .attr(
         'y',
-        Math.cos((2 / numPoints) * Math.PI * 2 + Math.PI) * radius +
-          centerX +
-          20,
+        Math.cos((2 / numPoints) * Math.PI * 2 + Math.PI) * radius + centerX,
       )
       .text(axisConfig[2]);
     svg
@@ -189,15 +174,13 @@ function RadarChart({ datas, kinds, width, height }) {
       .attr('text-anchor', 'middle')
       .attr(
         'x',
-        Math.sin((3 / numPoints) * Math.PI * 2 + Math.PI) * radius +
-          centerX +
-          25,
+        Math.sin((3 / numPoints) * Math.PI * 2 + Math.PI) * radius + centerX,
       )
       .attr(
         'y',
         Math.cos((3 / numPoints) * Math.PI * 2 + Math.PI) * radius +
           centerX +
-          20,
+          25,
       )
       .text(axisConfig[3]);
     svg
@@ -221,14 +204,16 @@ function RadarChart({ datas, kinds, width, height }) {
         'x',
         Math.sin((5 / numPoints) * Math.PI * 2 + Math.PI) * radius +
           centerX +
-          40,
+          30,
       )
       .attr(
         'y',
-        Math.cos((5 / numPoints) * Math.PI * 2 + Math.PI) * radius + centerX,
+        Math.cos((5 / numPoints) * Math.PI * 2 + Math.PI) * radius +
+          centerX +
+          10,
       )
       .text(axisConfig[5]);
-  }, [axisConfig, centerX, centerY, lines, radius]);
+  }, [axisConfig, centerX, centerY, polygonPoints, radius]);
 
   return (
     <div className="blocks__performance">

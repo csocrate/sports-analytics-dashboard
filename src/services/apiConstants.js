c0878167f698b167c3e99cfef663d9406159dec8
Devmatrix <devmatrix73@gmail.com>
2024-01-08 17:50:31 +0100

    Closes #6 - Add average sessions data from /user/:id/average-sessions - US#7

--- src/services/apiConstants.js
+++ src/services/apiConstants.js
@@ -7,3 +7,8 @@ export const userActivityUri = (id) => {
   const uri = `user/${id}/activity`;
   return uri;
 };
+      
+export const userAverageSessionsUri = (id) => {
+  const uri = `user/${id}/average-sessions`;
+  return uri;
+};

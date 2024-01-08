941eef783fe56ce897302231a069fbb139d077bf
Devmatrix <devmatrix73@gmail.com>
2024-01-08 17:41:17 +0100

    Closes #5 - Add user daily activity data from /user/:id/activity - US#6

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,6 @@
 import { useMockedDatas } from '../../hooks/useMockedDatas';
 import {
-  USER_ACTIVITY,
+  // USER_ACTIVITY,
   USER_AVERAGE_SESSIONS,
   // USER_MAIN_DATA,
   USER_PERFORMANCE,
@@ -11,17 +11,19 @@ import LineChart from '../../components/LineChart';
 import GroupedBarChart from '../../components/GroupedBarChart';
 import RadarChart from '../../components/RadarChart';
 import { useDatasApi } from '../../hooks/useDatasApi';
-import { userMainDataUri } from '../../services/apiConstants';
+import { userActivityUri, userMainDataUri } from '../../services/apiConstants';
 
 const USER_ID = 18; // Try 12 or 18
 const USER_MAIN_URI = userMainDataUri(USER_ID);
+const USER_ACTIVITY_URI = userActivityUri(USER_ID);
 
 function Profile() {
   // const { data: userMainData } = useMockedDatas(USER_ID, USER_MAIN_DATA);
   const { data: userSessions } = useMockedDatas(USER_ID, USER_AVERAGE_SESSIONS);
-  const { data: userActivity } = useMockedDatas(USER_ID, USER_ACTIVITY);
+  // const { data: userActivity } = useMockedDatas(USER_ID, USER_ACTIVITY);
   const { data: userPerformance } = useMockedDatas(USER_ID, USER_PERFORMANCE);
   const userMainData = useDatasApi(USER_MAIN_URI);
+  const userActivity = useDatasApi(USER_ACTIVITY_URI);
 
   const firstName = userMainData && userMainData.userInfos.firstName;
   const keyData = userMainData && userMainData.keyData;
--- src/services/apiConstants.js
+++ src/services/apiConstants.js
@@ -2,3 +2,8 @@ export const userMainDataUri = (id) => {
   const uri = `user/${id}`;
   return uri;
 };
+
+export const userActivityUri = (id) => {
+  const uri = `user/${id}/activity`;
+  return uri;
+};

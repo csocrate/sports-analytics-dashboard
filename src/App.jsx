20237f6f9a6865e2894ccb8a40072177947e6f8d
Devmatrix <devmatrix73@gmail.com>
2024-01-25 14:39:24 +0100

    Add user error message + update RouterProvider to allow user access to application root

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       {
-        path: 'tableau-de-bord',
+        path: '',
         element: <UserDashboard />,
       },
     ],
